21e501b75f3c14812cc35862ed3fb16262b5f385
Alexander Eremenko <relayne@Alexanders-MacBook-Air.local>
2026-09-17 07:49:58 +0300

    Next build b10-03c77af: early balance polygon (mission01_v2), forge chain SN2 (forge mode off in demo), door deadlock fix

--- next/index.service.worker.js
+++ next/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789611405|1354024';
+const CACHE_VERSION = '1789620595|1450382';
 /** @type {string} */
 const CACHE_PREFIX = 'Strategy Prototy-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
